5315dc652ac9db886171f4d171ec8eaf0a9fc209
Henrik Falck Mentzoni <henrik@henrikfalck.no>
2020-04-13 11:05:00 +0200

    Added christmas tree and styling

---
+++ src/components/ascii/christmasTree/tree.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./christmasTree.scss"
+
+// prettier-ignore
+const ChristmasTree = () => (
+  <div className="animated christmastree">
+    <p>
+            <span className="yellow">\ /</span><br/>
+           <span className="yellow">— * —</span><br/>
+            /<span className="light">●</span>\<br/>
+           /_\_\<br/>
+          /_/_<span className="light">●</span>_\<br/>
+         /_<span className="light">●</span>_\_\_\<br/>
+        /_/_/_/_/<span className="light">●</span>\<br/>
+       /<span className="light">●</span>\_\_\<span className="light">●</span>\_\_\<br/>
+      /_/_/<span className="light">●</span>/_/_/_/_\<br/>
+     /_\_\_\_\_\<span className="light">●</span>\_\_\<br/>
+    /_/<span className="light">●</span>/_/_/_<span className="light">●</span>_/_/<span className="light">●</span>/_\<br/>
+   /_\<span className="light">●</span>\_\_\_\_\<span className="light">●</span>\_\_\_\<br/>
+  /_/_/_/_/<span className="light">●</span>/_/_/_/_/<span className="light">●</span>/_\<br/>
+          <span className="pot">░░░░</span>
+    </p>
+  </div>
+)
+
+export default ChristmasTree
